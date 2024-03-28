2941fe9ed1ae60dfbc9384a651c0e53a7623bf61
kyemhee <khyoo0825@gmail.com>
2024-03-28 17:36:13 +0900

    test

--- pc2.py
+++ pc2.py
@@ -6,21 +6,10 @@ import yaml
 from PIL import Image
 from ultralytics import YOLO
 import torch
+import threading
+import queue
 
-def main():
-
-    model = YOLO("best_v8.pt")
-
-    with open("data.yaml") as file:
-        data = yaml.load(file, Loader= yaml.FullLoader)
-        HOST_IP = data["ip"]
-        PORT = data["port"]
-
-
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((HOST_IP, PORT))
-
-
+def receive_frame(client_socket, frame_queue):
     data = b""
     payload_size = struct.calcsize(">L")
     while True:
@@ -37,17 +26,46 @@ def main():
         frame_data = data[:msg_size]
         data = data[msg_size:]
 
-        frame = cv2.imdecode(np.frombuffer(frame_data, np.uint8), cv2.IMREAD_COLOR)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        print(type(frame))
+        frame_queue.put(frame_data)
+
+def process_and_display(model, frame_queue):
+    while True:
+        if not frame_queue.empty():
+            frame_data = frame_queue.get()
+            frame = cv2.imdecode(np.frombuffer(frame_data, np.uint8), cv2.IMREAD_COLOR)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            
+            results = model(frame)
         
-        results = model(frame)
+            cv2.imshow('frame', frame)
+            
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'):
+                break
 
-        # cv2.imshow('frame', frame)
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('q'):
-            break
+def main():
+    model = YOLO("best_v8.pt")
 
+    with open("data.yaml") as file:
+        data = yaml.load(file, Loader=yaml.FullLoader)
+        HOST_IP = data["ip"]
+        PORT = data["port"]
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((HOST_IP, PORT))
+
+    frame_queue = queue.Queue()
+
+    # 서버로부터 이미지 데이터를 받는 스레드
+    receive_thread = threading.Thread(target=receive_frame, args=(client_socket, frame_queue))
+    receive_thread.start()
+
+    # 받은 이미지 데이터를 처리하고 표시하는 스레드
+    display_thread = threading.Thread(target=process_and_display, args=(model, frame_queue))
+    display_thread.start()
+
+    receive_thread.join()
+    display_thread.join()
 
     client_socket.close()
     cv2.destroyAllWindows()
