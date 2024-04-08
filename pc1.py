56925378b0d6d97f344ce7c422a778ba70f35202
cccsssshh <shcho9666@gmail.com>
2024-04-08 16:02:28 +0900

    add thread function

--- pc1.py
+++ pc1.py
@@ -81,7 +81,7 @@ def main():
             break 
 
         pixel_data = server_socket.recv()
-        if pixel_data is not None:
+        if pixel_data["action"] == "action":
             from_x = pixel_data["from_x"]
             from_y = pixel_data["from_y"]
             to_x = pixel_data["to_x"]
--- pc2.py
+++ pc2.py
@@ -31,118 +31,50 @@ class WindowClass(QMainWindow, from_class):
         super().__init__()
         self.setupUi(self)
         self.setWindowTitle("CHESS AI")
-        self.client_thread = ClienThread()
+        self.client_thread = ClientThread()
+        self.rl_thread = RL_Thread()
+        
         self.client_thread.updateImage.connect(self.updateImage)
+        self.client_thread.toThread2.connect(self.rl_thread.action)
+        self.rl_thread.toThread1.connect(self.client_thread.update_action)
+
         self.client_thread.start()
-    
+        self.rl_thread.start()
+
     def updateImage(self, image):
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         h, w, ch = image.shape
         bytes_per_line = ch * w
         convert_to_Qt_format = QtGui.QImage(image.data.tobytes(), w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
-        p = convert_to_Qt_format.scaled(540, 540, Qt.KeepAspectRatio)
+        p = convert_to_Qt_format.scaled(630, 630, Qt.KeepAspectRatio)
         self.chessScreen.setPixmap(QtGui.QPixmap.fromImage(p))
-        
-    def mycurrentMove(self, action):
-        split_position = 2 
-        move = action
-        first_part = move[:split_position]
-        second_part = move[split_position:]
-        self.mymove.setText(f"{first_part}  to {second_part}")
-        move = f"Addin-Pago: {first_part}  to {second_part}"
-        myWindows.gameLog(move)
 
-    def aicurrentMove(self, changes):
-        split_position = 2 
-        move = changes
-        first_part = move[:split_position]
-        second_part = move[split_position:]
-        self.compmove.setText(f"{first_part} to {second_part}")
-        move = f"Chess.com: {first_part} to {second_part}"
-        myWindows.gameLog(move)
-    
-    def gameLog(self, moves):
-        current_text = self.gamehistory.text()
-        text = current_text+'\n' + moves
-        self.gamehistory.setText(text)
 
-    def captured(self, cur_piece, prev_piece):
-        piece_map = {
-            "p": "Black Pawn", "r": "Black Rook", "n": "Black Knight", 
-            "b": "Black Bishop", "k": "Black King", "q": "Black Queen",
-            "P": "White Pawn", "R": "White Rook", "N": "White Knight", 
-            "B": "White Bishop", "K": "White King", "Q": "White Queen"
-        }
-
-        # 문자열로 변환
-        cur_piece_str = str(cur_piece)
-        prev_piece_str = str(prev_piece)
-
-        # 말의 심볼을 이름으로 변환, 맵에 없는 경우 'Unknown piece'로 처리
-        current = str(piece_map.get(cur_piece_str, "Unknown piece"))
-        previous = str(piece_map.get(prev_piece_str, "Unknown piece"))
-
-        if previous.islower():
-            current_text = self.blackpieces.text()
-            text = current_text + previous + ','
-            self.blackpieces.setText(text)
-        else:
-            current_text = self.whitepieces.text()
-            text = current_text + previous + ','
-            self.whitepieces.setText(text)
-
-class ClienThread(QThread):
+class ClientThread(QThread):
     updateImage = pyqtSignal(np.ndarray)
+    toThread2 = pyqtSignal(dict)
 
     def __init__(self):
         super().__init__()
         with open("data.yaml") as file:
             self.yaml_data = yaml.load(file, Loader=yaml.FullLoader)
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect((self.yaml_data["ip"], self.yaml_data["port"]))
-        self.yolo_model = YOLO(self.yaml_data["yolo_model_path"])
-        self.config = Config()
-        self.chess_model = ChessModel(self.config)
-        self.chess_model.build()
-        self.chess_model.load(self.yaml_data["chess_model_path"])
-        self.env = ChessEnv().reset()
-        self.img = Img()
-        self.chess_player = ChessPlayer(self.config, self.chess_model.get_pipes(self.config.play.search_threads))
-        self.box_annotator = BoxAnnotator(color = ColorPalette.default(), thickness = 2, text_thickness = 1, text_scale = 0.3)
-        if not self.chess_model.load(self.yaml_data["chess_model_path"]):
-            raise RuntimeError("Failed to load the trained model weights")
+            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client_socket.connect((self.yaml_data["ip"], self.yaml_data["port"]))
+            self.yolo_model = YOLO(self.yaml_data["yolo_model_path"])
+
+        self.box_annotator = BoxAnnotator(color = ColorPalette.default(), thickness = 4, text_thickness = 2, text_scale = 0.7)
+        self.pixel_data = {"action" : "waiting"}
+        self.result_available = False  
 
     def run(self):
-        class_mapping = {
-        0: "chessboard",
-        1: "b",
-        2: "k",
-        3: "n",
-        4: "p",
-        5: "q",
-        6: "r",
-        7: "B",
-        8: "K",
-        9: "N",
-        10: "P",
-        11: "Q",
-        12: "R"
-        }
-        count = 0
         x_left = 0
         y_top = 0
         width = 0
         height = 0
-        prev_yolo_fen = None
-        yolo_fen = None
-        is_turn = True
-        is_moving = False
-        start = False
-        changes = None
-        action_flag = False
 
         data = b""
         payload_size = struct.calcsize(">L")
+
 
         while True:
             while len(data) < payload_size:
@@ -166,13 +98,24 @@ class ClienThread(QThread):
             ret, otsu = cv2.threshold(gray, -1,255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
             contours, hierarchy = cv2.findContours(otsu, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE) 
             
-            results = self.yolo_model.predict(source= frame, classes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], verbose=False, conf= 0.6)
+            results = self.yolo_model.predict(source= frame, classes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], verbose=False, conf= 0.65)
             boxes = results[0].boxes
             xywh_np = boxes.xywh.cpu().numpy()
             xyxy_np = boxes.xyxy.cpu().numpy()
             classes_np = boxes.cls.cpu().numpy()
             confidence_np =  boxes.conf.cpu().numpy()
             class_name = results[0].names
+
+            results_dict = {
+            "xywh": xywh_np,
+            "classes": classes_np,
+            "class_name": class_name,
+            "x_left" : x_left,
+            "y_top" : y_top,
+            "width" : width
+            }
+            self.toThread2.emit(results_dict)
+
             detections = Detections(
                 xyxy = xyxy_np,
                 confidence = confidence_np,
@@ -196,95 +139,159 @@ class ClienThread(QThread):
                         cropped = frame[y_top : y_top + height, x_left : x_left + width]
                         self.updateImage.emit(cropped)
 
-
-            vectorized_mapping = np.vectorize(class_mapping.get, otypes= [str])
-
-            mapped_classes_np = vectorized_mapping(classes_np)
-            xy_np = xywh_np[:, :2]
-            positions = []
-            for x,y in xy_np:
-                position = pixel_to_chess_coord(int(x), int(y), (x_left, y_top), 100)
-                positions.append(position)
-            positions = np.array(positions)
-            pieces_positions = dict(zip(positions, mapped_classes_np))
-
-            if yolo_fen is not None:
-                prev_yolo_fen = yolo_fen
-
-            yolo_fen = create_fen_from_positions(pieces_positions)
-            chess_module_fen = self.env.board.fen().split(' ')[0]
-
-            if yolo_fen == chess_module_fen:
-                start = True
-
-            if start == True:
-                if is_turn == True and yolo_fen == chess_module_fen and is_moving == False:
-                    action = self.chess_player.action(self.env, False)
-                    print(f"white moves : {action}")
-                    prev_pos = action[2:4]
-                    prev_pos = chess.parse_square(prev_pos)
-                    prev_piece = self.env.board.piece_at(prev_pos)
-                    self.env.step(action)
-                    action_flag = True
-                    cur_pos = action[2:4]
-                    cur_pos = chess.parse_square(cur_pos)
-                    cur_piece = self.env.board.piece_at(cur_pos)
-                    from_x, from_y, to_x, to_y = self.img.automouse(action, (x_left, y_top), width // 8)
-                    pixel_data = {"from_x" : from_x, "from_y" : from_y, "to_x" : to_x, "to_y" : to_y}
-                    is_moving = True
-                    is_turn = False
-                    if prev_piece != None and prev_piece != cur_piece:
-                        print(f"{cur_piece}가 {prev_piece}를 잡았습니다.")
-                elif is_moving == True and yolo_fen != chess_module_fen:
-
-                    is_turn = False
-                elif is_turn == False and chess_module_fen== yolo_fen:
-                    is_moving = False
-                elif is_turn == False and chess_module_fen!= yolo_fen and is_moving == False:
-                    
-                    if prev_yolo_fen == yolo_fen:
-                        count += 1
-                    if count > 10:
-                        changes = compare_positions(yolo_fen, chess_module_fen)
-                        if chess.Move.from_uci(changes) in self.env.board.legal_moves:
-                            counter = Counter(list(results[0].boxes.cls.cpu().numpy()))
-                            prev_classes_number = list(counter.items())
-                            prev_classes_number = dict(map(lambda x: (class_name[x[0]],x[1]),prev_classes_number))
-                            prev_pos = changes[2:4]
-                            prev_pos = chess.parse_square(prev_pos)
-                            prev_piece = self.env.board.piece_at(prev_pos)
-                            if is_promotion_move(changes, self.env.board):
-                                print(f"Promotion detected: {changes}")
-                                counter = Counter(list(results[0].boxes.cls.cpu().numpy()))
-                                cur_classes_number = list(counter.items())
-                                cur_classes_number = dict(map(lambda x: (class_name[x[0]],x[1]),cur_classes_number))
-                                promotion = identify_promotion(prev_classes_number, cur_classes_number)
-                                changes += promotion  
-                            print(f"black moves : {changes}")
-                            self.env.step(changes)
-                            cur_pos = changes[2:4]
-                            cur_pos = chess.parse_square(cur_pos)
-                            cur_piece = self.env.board.piece_at(cur_pos)
-                            is_turn = True
-                            if prev_piece != None and prev_piece != cur_piece:
-                                print(f"{cur_piece}가 {prev_piece}를 잡았습니다.")
-
-                        count = 0
-                if self.env.board.is_game_over():
-                    print("game is over")
-                    print(self.env.board.result())
-                    break
-            if action_flag == True:
-                json_pixel_data = json.dumps(pixel_data) 
-                self.client_socket.sendall(json_pixel_data.encode('utf-8'))
-                action_flag = False
+            if self.result_available:
+                self.send(self.pixel_data)
+                self.result_available = False
+                self.pixel_data = {"action": "waiting"}
             else:
-                json_pixel_data = json.dumps("None")
-                self.client_socket.sendall(json_pixel_data.encode('utf-8'))
+                self.send({"action": "waiting"})
     
+    def update_action(self, result):
+        self.pixel_data = result
+        self.result_available = True
+
+    def send(self, pixel_data):
+        try:
+            json_pixel_data = json.dumps(pixel_data)
+            self.client_socket.sendall(json_pixel_data.encode('utf-8'))
+        except BrokenPipeError as e:
+            print(f"Error: {e}")
+
     def stop(self):
         self.client_socket.close()
         self.terminate()
+
+
+class RL_Thread(QThread):
+    toThread1 = pyqtSignal(dict)
+
+    def __init__(self):
+        super().__init__() 
+        with open("data.yaml") as file:
+            self.yaml_data = yaml.load(file, Loader=yaml.FullLoader)
+            self.yolo_model = YOLO(self.yaml_data["yolo_model_path"])
+
+        self.config = Config()
+        self.chess_model = ChessModel(self.config)
+        self.chess_model.build()
+        self.chess_model.load(self.yaml_data["chess_model_path"])
+        self.env = ChessEnv().reset()
+        self.chess_player = ChessPlayer(self.config, self.chess_model.get_pipes(self.config.play.search_threads))
+        self.img = Img()
+
+        if not self.chess_model.load(self.yaml_data["chess_model_path"]):
+            raise RuntimeError("Failed to load the trained model weights")
+
+        self.class_mapping = {
+        0: "chessboard",
+        1: "b",
+        2: "k",
+        3: "n",
+        4: "p",
+        5: "q",
+        6: "r",
+        7: "B",
+        8: "K",
+        9: "N",
+        10: "P",
+        11: "Q",
+        12: "R"
+        }
+        self.count = 0
+        self.prev_yolo_fen = None
+        self.yolo_fen = None
+        self.is_turn = True
+        self.is_moving = False
+        self.is_start = False
+        self.changes = None
+        self.action_flag = False
+        self.pixel_data = {}
+
+    def action(self, results_dict):
+        xywh_np = results_dict["xywh"]
+        classes_np = results_dict["classes"]
+        class_name = results_dict["class_name"]
+        x_left = results_dict["x_left"]
+        y_top = results_dict["y_top"]
+        width = results_dict["width"]
+
+        vectorized_mapping = np.vectorize(self.class_mapping.get, otypes= [str])
+        mapped_classes_np = vectorized_mapping(classes_np)
+        xy_np = xywh_np[:, :2]
+        positions = []
+        for x,y in xy_np:
+            position = pixel_to_chess_coord(int(x), int(y), (x_left, y_top), 100)
+            positions.append(position)
+        positions = np.array(positions)
+        pieces_positions = dict(zip(positions, mapped_classes_np))
+
+        if self.yolo_fen is not None:
+            self.prev_yolo_fen = self.yolo_fen
+
+        self.yolo_fen = create_fen_from_positions(pieces_positions)
+        chess_module_fen = self.env.board.fen().split(' ')[0]
+
+        if self.yolo_fen == chess_module_fen:
+            self.is_start = True
+
+        if self.is_start == True:
+            if self.is_turn == True and self.yolo_fen == chess_module_fen and self.is_moving == False:
+                action = self.chess_player.action(self.env, False)
+                print(f"white moves : {action}")
+                prev_pos = action[2:4]
+                prev_pos = chess.parse_square(prev_pos)
+                prev_piece = self.env.board.piece_at(prev_pos)
+                self.env.step(action)
+                self.action_flag = True
+                cur_pos = action[2:4]
+                cur_pos = chess.parse_square(cur_pos)
+                cur_piece = self.env.board.piece_at(cur_pos)
+                from_x, from_y, to_x, to_y = self.img.automouse(action, (x_left, y_top), width // 8)
+                self.pixel_data = {"action" : "action", "from_x" : from_x, "from_y" : from_y, "to_x" : to_x, "to_y" : to_y}
+                self.is_moving = True
+                self.is_turn = False
+                if prev_piece != None and prev_piece != cur_piece:
+                    print(f"{cur_piece}가 {prev_piece}를 잡았습니다.")
+            elif self.is_moving == True and self.yolo_fen != chess_module_fen:
+                self.is_turn = False
+            elif self.is_turn == False and chess_module_fen== self.yolo_fen:
+                self.is_moving = False
+            elif self.is_turn == False and chess_module_fen!= self.yolo_fen and self.is_moving == False:
+                if self.prev_yolo_fen == self.yolo_fen:
+                    self.count += 1
+                if self.count > 10:
+                    self.changes = compare_positions(self.yolo_fen, chess_module_fen)
+                    if chess.Move.from_uci(self.changes) in self.env.board.legal_moves:
+                        counter = Counter(list(classes_np))
+                        prev_classes_number = list(counter.items())
+                        prev_classes_number = dict(map(lambda x: (class_name[x[0]],x[1]),prev_classes_number))
+                        prev_pos = self.changes[2:4]
+                        prev_pos = chess.parse_square(prev_pos)
+                        prev_piece = self.env.board.piece_at(prev_pos)
+                        if is_promotion_move(self.changes, self.env.board):
+                            print(f"Promotion detected: {self.changes}")
+                            counter = Counter(list(classes_np))
+                            cur_classes_number = list(counter.items())
+                            cur_classes_number = dict(map(lambda x: (class_name[x[0]],x[1]),cur_classes_number))
+                            promotion = identify_promotion(prev_classes_number, cur_classes_number)
+                            self.changes += promotion  
+                        print(f"black moves : {self.changes}")
+                        self.env.step(self.changes)
+                        cur_pos = self.changes[2:4]
+                        cur_pos = chess.parse_square(cur_pos)
+                        cur_piece = self.env.board.piece_at(cur_pos)
+                        self.is_turn = True
+                        if prev_piece != None and prev_piece != cur_piece:
+                            print(f"{cur_piece}가 {prev_piece}를 잡았습니다.")
+
+                    self.count = 0
+            if self.env.board.is_game_over():
+                print("game is over")
+                print(self.env.board.result())
+
+        if self.action_flag == True:
+            self.toThread1.emit(self.pixel_data)
+            self.action_flag = False
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
